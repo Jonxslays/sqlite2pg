6ebc9fe920148aa43bf7719208e6243f7116d277
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-09-05 21:04:06 -0700

    Bump version number

--- sqlite2pg/__init__.py
+++ sqlite2pg/__init__.py
@@ -1,3 +1,9 @@
 """An Sqlite3 to Postgresql database migration tool."""
 from .modules import *
 from .cli import main
+
+__version__ = "0.1.1"
+__author__ = "Jonxslays"
+__maintainer__ = "Jonxslays"
+__license__ = "BSD-3-Clause"
+__url__ = "https://github.com/Jonxslays/sqlite2pg"
